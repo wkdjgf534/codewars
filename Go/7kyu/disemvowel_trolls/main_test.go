51806f1fe235aa33e49cbe275d7b15f699287828
Eugene Makarikhin <contact@aboutslack.info>
2024-06-15 22:20:55 +0200

    Update unit tests for 7kyu Go tasks

--- Go/7kyu/disemvowel_trolls/main_test.go
+++ Go/7kyu/disemvowel_trolls/main_test.go
@@ -1,26 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestDisemvowel(t *testing.T) {
-	var tests = []struct {
+	type args struct {
 		comment string
-		want    string
-	}{
-		{"This is a table", "Ths s  tbl"},
-		{"SLACKWARE", "SLCKWR"},
-		{"rest api", "rst p"},
-		{"pAnCaKe wItH TeA", "pnCK wtH T"},
 	}
-
-	for _, test := range tests {
-		testName := fmt.Sprintf("%s", test.comment)
-		t.Run(testName, func(t *testing.T) {
-			if result := Disemvowel(test.comment); result != test.want {
-				t.Errorf("got %s, want %s", result, test.want)
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "when string contains vowel letters",
+			args: args{comment: "Hello there"},
+			want: "Hll thr",
+		},
+		{
+			name: "when string does not contain vowel letters",
+			args: args{comment: "TTTTT, rrrrr"},
+			want: "TTTTT, rrrrr",
+		},
+		{
+			name: "when an empty string",
+			args: args{comment: ""},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Disemvowel(tt.args.comment); got != tt.want {
+				t.Errorf("Disemvowel() = %v, want %v", got, tt.want)
 			}
 		})
 	}
--- Go/7kyu/vowel_count/main_test.go
+++ Go/7kyu/vowel_count/main_test.go
@@ -1,26 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetCount(t *testing.T) {
-	var tests = []struct {
-		str  string
-		want int
-	}{
-		{"vowel", 2},
-		{"SLACKWARE", 3},
-		{"rest api", 3},
-		{"pAnCaKe with tea", 6},
+	type args struct {
+		str string
 	}
-
-	for _, test := range tests {
-		testName := fmt.Sprintf("%s", test.str)
-		t.Run(testName, func(t *testing.T) {
-			if result := GetCount(test.str); result != test.want {
-				t.Errorf("got %d, want %d", result, test.want)
+	tests := []struct {
+		name      string
+		args      args
+		wantCount int
+	}{
+		{
+			name:      "when a word contains vowel letters",
+			args:      args{str: "vowel"},
+			wantCount: 2,
+		},
+		{
+			name:      "when a word does not contain vowel letters",
+			args:      args{str: "sssss"},
+			wantCount: 0,
+		},
+		{
+			name:      "when an empty string",
+			args:      args{str: ""},
+			wantCount: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotCount := GetCount(tt.args.str); gotCount != tt.wantCount {
+				t.Errorf("GetCount() = %v, want %v", gotCount, tt.wantCount)
 			}
 		})
 	}
